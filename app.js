8558d175e5a7500f9abb71f56d0973d92cf4a6bc
sabeelmhd <sabeelzaim@gmail.com>
2024-03-03 12:33:05 +0530

    wwebServer.js

--- app.js
+++ app.js
@@ -13,3 +13,13 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
   if (err) throw err
   console.log('File saved!')
 })
+
+//webServer.js
+const http = require('http')
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write('Hello World!')
+    res.end()
+  })
+  .listen(8080)
